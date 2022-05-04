5769c62ac4b8f376072b9a69f6d5317e79933f36
Ilchenko Vladyslav <prodigyvlad@gmail.com>
2022-05-04 22:39:15 +0300

    Change message max length to 2048

--- AlertsApi.Infrastructure/Configurations/MessagesConfiguration.cs
+++ AlertsApi.Infrastructure/Configurations/MessagesConfiguration.cs
@@ -9,7 +9,7 @@ public class MessagesConfiguration : IEntityTypeConfiguration<DbMessage>
     public void Configure(EntityTypeBuilder<DbMessage> builder)
     {
         builder.HasKey(alert => alert.Id);
-        builder.Property(alert => alert.Message).IsRequired().HasMaxLength(500);
+        builder.Property(alert => alert.Message).IsRequired().HasMaxLength(2048);
         builder.Property(alert => alert.Date).IsRequired();
     }
 }
---
+++ AlertsApi.Infrastructure/Migrations/20220504193652_ChangeMessageConstraint.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlertsApi.Infrastructure.Migrations
+{
+    public partial class ChangeMessageConstraint : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                table: "Message",
+                type: "character varying(2048)",
+                maxLength: 2048,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(500)",
+                oldMaxLength: 500);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                table: "Message",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(2048)",
+                oldMaxLength: 2048);
+        }
+    }
+}
